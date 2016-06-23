93dd3cc1da93d1020740a2f90112c8f6bd307110
Brandon Roberts <bmroberts1987@users.noreply.github.com>
2016-06-22 23:13:25 -0400

    Add files via upload

---
+++ Document2.js
@@ -0,0 +1,16 @@
+var MyComponent = React.createClass({
+	render: function(){
+		return (
+			<p>
+			<h2>Paragraph #1</h2>
+			this is a long html script
+			it is not ideal for jsx because:
+			<u1>
+			<li>it is too long to be embedded in a .js file</li>
+			<li>it is not reusable</li>
+			</u1>
+		   </p>
+		):
+
+	}
+})
